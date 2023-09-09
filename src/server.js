58352690efef8a318b5aa726d31396ae6985cb07
Luis Guzman <guzmanluis.lg@gmail.com>
2023-09-09 11:45:48 -0500

    solve cors problem

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 require('dotenv').config();
 const authRoutes = require('./routes/authRoutes');
+const cors = require('cors');
 
 const app = express();
 
 const port = process.env.PORT;
+
+app.use(cors());
 
 app.use(express.json()); // Middleware to parse JSON requests
 
