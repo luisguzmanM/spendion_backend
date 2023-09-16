ea10a71de42ed50a54c1c285a325cee762cd2142
Luis Guzman <guzmanluis.lg@gmail.com>
2023-09-16 17:26:26 -0500

    update

--- src/utils/calcs.js
+++ src/utils/calcs.js
@@ -4,12 +4,10 @@ const getValuesByBudget = arr => {
       budget.spent = budget.record.reduce((acc, elem) => acc + elem.amount, 0);
       budget.free = budget.spent <= budget.amount ? budget.amount - budget.spent : 0;
       budget.progress = budget.spent <= budget.amount ? Math.ceil((budget.spent * 100) / budget.amount) : 100;
-      console.log(budget);
     } else {
       budget.spent = 0;
       budget.free = budget.amount;
       budget.progress = 0;
-      console.log(budget);
     }
   })
   return arr;
