25d1714ecbc86363a05df8486a411e5ba231eec3
Luis Guzman <guzmanluis.lg@gmail.com>
2023-10-06 16:30:34 -0500

    update

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.CONNECTIONSTRING,
+  connectionString: process.env.DATABASE_URL,
   ssl: true,
 })
 
