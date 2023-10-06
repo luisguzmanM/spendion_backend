fe81e784553378eddbe2d2b1d577b39d568e1016
Luis Guzman <guzmanluis.lg@gmail.com>
2023-10-06 15:55:34 -0500

    update

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -8,13 +8,13 @@ const getBudgets = async (req, res) => {
   let result;
   try {
     result = await db.query(models.getBudgets, [id_person]);
+    let budgets = result.rows[0].get_all_budgets_by_id_person === null ? [] : result.rows[0].get_all_budgets_by_id_person;
+    budgets = getValuesByBudget(budgets);
+    res.status(200).send(budgets);
   } catch (err) {
     console.log(err);
     res.status(500).send({msj: 'Error getting budgets'});
   }
-  let budgets = result.rows[0].get_all_budgets_by_id_person === null ? [] : result.rows[0].get_all_budgets_by_id_person;
-  budgets = getValuesByBudget(budgets);
-  res.status(200).send(budgets);
 }
 
 const createBudget = async (req, res) => {
@@ -23,20 +23,20 @@ const createBudget = async (req, res) => {
   let result;
   try {
     result = await db.query(models.createBudget, [person.id_person, title, amount])
+    const budget = {
+      amount: result.rows[0].budget.amount,
+      free: result.rows[0].budget.amount,
+      id_budget: result.rows[0].budget.id_budget,
+      progress: 0,
+      record: null,
+      spent: 0,
+      title: result.rows[0].budget.title
+    }
+    res.status(200).send(budget);
   } catch (err) {
     console.log(err);
     res.status(500).send({msj: 'Problem to create budget'});
   }
-  const budget = {
-    amount: result.rows[0].budget.amount,
-    free: result.rows[0].budget.amount,
-    id_budget: result.rows[0].budget.id_budget,
-    progress: 0,
-    record: null,
-    spent: 0,
-    title: result.rows[0].budget.title
-  }
-  res.status(200).send(budget);
 }
 
 const deleteBudget = async (req, res) => {
@@ -44,11 +44,11 @@ const deleteBudget = async (req, res) => {
   let result;
   try {
     await db.query(models.deleteBudget, [id_budget]);
+    res.status(200).send({msj: 'Budget deleted successfully'})
   } catch (err) {
     console.log(err);
     res.status(500).send({msj: 'Error removing budget'});
   }
-  res.status(200).send({msj: 'Budget deleted successfully'})
 }
 
 const updateRecord = async (req, res) => {
@@ -56,13 +56,13 @@ const updateRecord = async (req, res) => {
   let result;
   try {
     result = await db.query(models.updateRecord, [id_budget, JSON.stringify(record)]);
+    result.rows[0].update_record.record === null ? result.rows[0].update_record.record = [] : '';
+    const recordUpdated = result.rows[0].update_record.record;
+    res.status(200).send(recordUpdated);
   } catch (err) {
     console.log(err);
     res.status(500).send({msj: 'There is a problem to update record'});
   }
-  result.rows[0].update_record.record === null ? result.rows[0].update_record.record = [] : '';
-  const recordUpdated = result.rows[0].update_record.record;
-  res.status(200).send(recordUpdated);
 }
 
 const addIncome = async (req, res) => {
@@ -83,11 +83,11 @@ const getIncome = async (req, res) => {
   let result;
   try {
     result = await db.query(models.getIncome, [person.id_person]);
+    res.status(200).send({income: result.rows[0].income});
   } catch (err) {
     console.log(err)
     res.status(500).send({msj: 'Problem trying to get income'});
   }
-  res.status(200).send({income: result.rows[0].income});
 }
 
 module.exports = {
