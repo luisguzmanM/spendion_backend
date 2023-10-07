d202c7128235925390e90f9aaf3bfae0b007504f
Luis Guzman <guzmanluis.lg@gmail.com>
2023-10-07 15:43:52 -0500

    install path

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const authRoutes = require('./routes/authRoutes');
 const homeRouter = require('./routes/homeRoutes');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -15,5 +16,9 @@ app.use(express.json());
 app.use('/auth', authRoutes);
 
 app.use('/home', homeRouter);
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'https://spendion-backend.onrender.com/index.html'));
+});
 
 app.listen(port, () => console.log(`Server running in port ${port} :D`));
