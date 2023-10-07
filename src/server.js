5fbc767cb4bb4f3f26b5d4ad857aa0e29c9221ed
Luis Guzman <guzmanluis.lg@gmail.com>
2023-10-07 15:01:14 -0500

    update

--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const authRoutes = require('./routes/authRoutes');
 const homeRouter = require('./routes/homeRoutes');
 const cors = require('cors');
-const path = require('path');
 
 const app = express();
 
@@ -12,12 +11,6 @@ const port = process.env.PORT;
 app.use(cors());
 
 app.use(express.json());
-
-// Manejar las rutas no encontradas
-app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, 'spendion-frontend.onrender.com/index.html'));
-});
-
 
 app.use('/auth', authRoutes);
 
