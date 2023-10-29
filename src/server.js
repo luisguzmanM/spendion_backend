0a82fc86abb8ac038ad807699b82667baea3ce0f
Luis Guzman <guzmanluis.lg@gmail.com>
2023-10-28 21:01:19 -0500

    update

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const authRoutes = require('./routes/authRoutes');
 const homeRouter = require('./routes/homeRoutes');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -13,5 +14,13 @@ app.use(express.json());
 
 app.use('/auth', authRoutes);
 app.use('/home', homeRouter);
+
+// Configuración para servir archivos estáticos desde la carpeta "dist/spendion"
+app.use(express.static(path.join(__dirname, 'dist/spendion')));
+
+// Agrega una regla para redirigir todas las solicitudes a index.html
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/spendion/index.html'));
+});
 
 app.listen(port, () => console.log(`Server running in port ${port} :D`));
