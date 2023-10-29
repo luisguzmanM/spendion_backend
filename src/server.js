c82cd6038ab113f1038220864821de4d65474cee
Luis Guzman <guzmanluis.lg@gmail.com>
2023-10-28 20:20:53 -0500

    update

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 require('dotenv').config();
 const authRoutes = require('./routes/authRoutes');
 const homeRouter = require('./routes/homeRoutes');
@@ -14,13 +13,5 @@ app.use(express.json());
 
 app.use('/auth', authRoutes);
 app.use('/home', homeRouter);
-
-// Configuración para servir archivos estáticos de Angular
-app.use(express.static(path.join(__dirname, '../dist/spendion')));
-
-// Configuración para manejar rutas de Angular
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, '../dist/spendion', 'index.html'));
-});
 
 app.listen(port, () => console.log(`Server running in port ${port} :D`));
