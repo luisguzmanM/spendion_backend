dd858811ae9b8f7a0a49372ea6fe4a82859d2fac
Luis Guzman <guzmanluis.lg@gmail.com>
2023-11-18 17:49:05 -0500

    update

--- src/controllers/settingController.js
+++ src/controllers/settingController.js
@@ -5,8 +5,8 @@ const updateDataUser = async (req, res) => {
   const { id_person, fname, lname, photo } = req.body;
   try {
     const userDataUpdated = await db.query(model.updateDataUser, [id_person, fname, lname, photo]);
-    console.log(userDataUpdated.rows[0].person);
-    res.status(200).send({ msj: 'Updated', data: userDataUpdated.rows[0].person });
+    console.log(userDataUpdated.rows[0]);
+    res.status(200).send({ msj: 'Updated' });
   } catch (error) {
     res.status(500).send('Error trying to update profile');
   }
--- src/models/settingModel.js
+++ src/models/settingModel.js
@@ -1,4 +1,4 @@
-const updateDataUser = `SELECT * FROM public.update_person($1, $2, $3, $4) AS person`;
+const updateDataUser = `SELECT * FROM update_person($1, $2, $3, $4) AS person`;
 
 module.exports = {
   updateDataUser
